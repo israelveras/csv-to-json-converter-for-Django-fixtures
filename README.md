0b1336cdcdd26987985aa946b61c6b31a125b0c0
Israel Veras <israelveras@gmail.com>
2022-07-16 13:54:51 -0300

    v2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # csv-to-json-converter-for-Django-fixtures
+
+Two simple and practical algorithms that convert csv file into json in the format read by Django when loading fixtures.
+
+Algorithm csv_to_json_Django_fixtures.py generates the id incrementally without you having to reserve a column for id in the csv file.
+
+In csv_to_json_setting_id.py you need to reserve the first column of csv with the ids you want to set.
--- csv_to_json_setting_id.py
+++ csv_to_json_setting_id.py
@@ -5,21 +5,18 @@ csv_path = '/path-my-csv/file.csv'
 json_output_file = '/path-out/namefile.json'
 model_name = 'app.ModelName'
 
-def read_csv_write_json(csv_file, json_file, app_model):
+def read_csv_write_json_setting_id(csv_file, json_file, app_model):
     csv_row = []
     with open(csv_file) as csv_f:
         csv_r = csv.DictReader(csv_f)
         field = csv_r.fieldnames
-        id = 0
         for row in csv_r:
-            id + 1
-            csv_row.extend(
-                [{"model": app_model, "pk": id, "fields": {field[i]: row[field[i]] for i in range(len(field))}}])
-        converter_json(csv_row, json_output_file)
+            csv_row.extend([{"model": app_model, "pk": row[field[0]], "fields": {field[i]:row[field[i]] for i in range(1, len(field))}}])
+        convert_write_json(csv_row, json_output_file)
 
-def converter_json(data, json_file):
+def convert_write_json(data, json_file):
     with open(json_file, "w") as f:
         f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': ')))
         f.write(json.dumps(data))
 
-read_csv_write_json(csv_path, json_output_file, model_name)
+read_csv_write_json_setting_id(csv_path, json_output_file, model_name)
