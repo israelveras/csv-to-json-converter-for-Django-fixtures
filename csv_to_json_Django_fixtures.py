9a222ffd34383063a072e0849799653d8626654d
Israel Veras <israelveras@gmail.com>
2022-07-16 13:06:15 -0300

    v1

---
+++ csv_to_json_Django_fixtures.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+csv_path = '/my-path-csv/file.csv'
+json_output_file = '/path-out/file.json'
+model_name = 'app.ModelName'
+
+
+def read_csv_write_json(csv_file, json_file, appModel):
+    csv_row = []
+    with open(csv_file) as csv_f:
+        csv_r = csv.DictReader(csv_f)
+        field = csv_r.fieldnames
+        id = 0
+        for row in csv_r:
+            id = id + 1
+            csv_row.extend(
+                [{"model": appModel, "pk": id, "fields": {field[i]: row[field[i]] for i in range(len(field))}}])
+        converter_json(csv_row, json_output_file)
+
+
+def converter_json(data, json_file):
+    with open(json_file, "w") as f:
+        f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': ')))
+        f.write(json.dumps(data))
+
+read_csv_write_json(csv_path, json_output_file, model_name)
